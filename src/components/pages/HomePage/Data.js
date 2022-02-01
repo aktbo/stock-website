9dc40a41148708eff264fed5fc9767c2affbd564
Aaron Thibeaux <aktbomusic@gmail.com>
2022-01-31 23:28:39 -0600

    Adding Repository

---
+++ src/components/pages/HomePage/Data.js
@@ -0,0 +1,55 @@
+export const homeObjOne = {
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Stock Text",
+  headline: "Stock Stock Stock Stock",
+  description:
+    "Stock Stock Stock Stock Stock Stock Stock Stock Stock Stock Stock Stock Stock Stock Stock Stock Stock Stock Stock Stock",
+  buttonLabel: "Stock Button",
+  imgStart: "",
+  img: "./",
+  alt: "",
+};
+
+export const homeObjTwo = {
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Stock Text",
+  headline: "Stock Stock Stock Stock",
+  description:
+    "Stock Stock Stock Stock Stock Stock Stock Stock Stock Stock Stock Stock Stock Stock Stock Stock Stock Stock Stock Stock",
+  buttonLabel: "Stock Button",
+  imgStart: "",
+  img: "./",
+  alt: "",
+};
+
+export const homeObjThree = {
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Stock Text",
+  headline: "Stock Stock Stock Stock",
+  description:
+    "Stock Stock Stock Stock Stock Stock Stock Stock Stock Stock Stock Stock Stock Stock Stock Stock Stock Stock Stock Stock",
+  buttonLabel: "Stock Button",
+  imgStart: "",
+  img: "./",
+  alt: "",
+};
+
+export const homeObjFour = {
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Stock Text",
+  headline: "Stock Stock Stock Stock",
+  description:
+    "Stock Stock Stock Stock Stock Stock Stock Stock Stock Stock Stock Stock Stock Stock Stock Stock Stock Stock Stock Stock",
+  buttonLabel: "Stock Button",
+  imgStart: "",
+  img: "./",
+  alt: "",
+};
